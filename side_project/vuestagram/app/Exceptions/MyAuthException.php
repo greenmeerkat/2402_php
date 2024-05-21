04b20f52578dece70327d62c2797812e29c9629d
greenmeerkat <milkpbj23@gmail.com>
2024-05-21 17:25:50 +0900

    240521 로그인 구현

---
+++ side_project/vuestagram/app/Exceptions/MyAuthException.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Exceptions;
+
+use Exception;
+
+class MyAuthException extends Exception {
+    /**
+     * 에러 메세지 리스트
+     * 
+     * @return Array 에러메세지 배열
+     */
+    public function context() {
+        return [
+            'E20' => ['status' => 401, 'msg' => '미등록 유저'],
+            'E21' => ['status' => 401, 'msg' => '비밀번호 비일치'],
+        ];
+    }
+}
